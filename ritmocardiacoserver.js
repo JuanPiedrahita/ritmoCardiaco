d5a0c9c884d56c0b040783f6fe7b4df8c876cdfc
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-03-31 21:01:37 -0500

    Initial server

---
+++ ritmocardiacoserver.js
@@ -0,0 +1,51 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var setHeaders = function(response){
+	response.header('Access-Control-Allow-Origin', '*');
+  	response.header('Access-Control-Allow-Methods', 'GET, POST');
+  	response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  	return response;
+}
+
+var lecturas = [];
+lecturas.push({
+	"tiempo":1,
+	"ritmo":1
+})
+
+var router = express.Router();
+
+router.get('/getLecturas', function (request, response) {
+  	response = setHeaders(response);
+  	response.contentType('application/json').status(200);
+  	response.send(JSON.stringify(lecturas));
+  	response.end;
+});
+
+router.post('/postLecturas', function (request, response) {
+	//console.log("params", request.query);
+	console.log("body", request.body);
+	//console.log("request", request);
+  	//console.log("data", request.query.data);
+	lecturas.push(JSON.parse(JSON.stringify(request.body)));  	
+  	response = setHeaders(response);
+  	response.send(JSON.stringify(lecturas));
+  	response.end;
+});
+
+app.use(router);
+
+var server = app.listen(4040, function () {
+  "use strict";
+
+  var host = '0.0.0.0',
+      port = server.address().port;
+
+  console.log(' Server is listening at http://%s:%s', host, port);
+});
